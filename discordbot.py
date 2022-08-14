4e4f8a1af596b86a212b2ed1cd687510620993dc
maki <71871601+makkenno@users.noreply.github.com>
2022-08-14 15:59:40 +0900

    本番サーバーテスト

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import traceback
 
 # https://discord.com/channels/1008227908617248798/1008227908617248802（この部分）
-CHANNEL_ID = 983245007429468193
+CHANNEL_ID = 988398446287405066
 
 client = discord.Client()
 
@@ -13,12 +13,13 @@ channel_sent = None
 
 
 # 60秒に一回ループ
-@tasks.loop(seconds=10)
+@tasks.loop(seconds=30)
 async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
     # 指定したい時刻から9時間引いた時刻
     # if now == '15:30':
+    await channel_sent.send(f'{member.mention} テスト')
     for ch in channel_sent.guild.voice_channels:
         for member in ch.members:
             # move_to(None)で特定のメンバーを切断する
