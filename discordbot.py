3b5065e138845a208cd674706b47421aadd52dbe
maki <71871601+makkenno@users.noreply.github.com>
2022-08-14 13:53:24 +0900

    ループテスト

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,6 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 from os import getenv
+from datetime import datetime
 import traceback
 
 bot = commands.Bot(command_prefix='/')
@@ -15,6 +16,18 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('やぴまる')
+    
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    # if now == '07:00':
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('おはよう')  
+
+#ループ処理実行
+loop.start()
 
 
 token = getenv('DISCORD_BOT_TOKEN')
