9aa0e0aaed0077a234dbe615f2e4744671e72d72
maki <71871601+makkenno@users.noreply.github.com>
2022-08-14 15:29:31 +0900

    インデント設定の修正

--- discordbot.py
+++ discordbot.py
@@ -23,7 +23,7 @@ async def loop():
     # if now == '15:30':
     await channel_sent.send(f'<@905072543919116308> おやすみなさい')
     for ch in channel_sent.guild.voice_channels:
-		for member in ch.members:
+        for member in ch.members:
 	    	# move_to(None)で特定のメンバーを切断する
 			await member.move_to(None)
             await channel_sent.send(f'{member}おやすみ')
