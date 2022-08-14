433c602bdbdd2f8ab39be29500be8a297022d21e
maki <71871601+makkenno@users.noreply.github.com>
2022-08-14 14:08:26 +0900

    ループのみ

--- discordbot.py
+++ discordbot.py
@@ -7,26 +7,13 @@ import traceback
 # https://discord.com/channels/1008227908617248798/1008227908617248802（この部分）
 CHANNEL_ID = 1008227908617248802
 
-bot = commands.Bot(command_prefix='/')
 client = discord.Client()
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('更新したよ')
     
 # 60秒に一回ループ
 @tasks.loop(seconds=10)
 async def loop():
     # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
+    # now = datetime.now().strftime('%H:%M')
     # if now == '07:00':
     channel = client.get_channel(CHANNEL_ID)
     await channel.send('10秒経ったよ')  
@@ -36,5 +23,4 @@ loop.start()
 
 
 token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
 client.run(token)
