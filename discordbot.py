a011c21a6aad941e1e1a3e8d8361e03905816295
maki <71871601+makkenno@users.noreply.github.com>
2022-08-14 16:59:42 +0900

    本番反映する & リファクタリング

--- discordbot.py
+++ discordbot.py
@@ -13,18 +13,19 @@ channel_sent = None
 
 
 # 60秒に一回ループ
-@tasks.loop(seconds=10)
+@tasks.loop(seconds=60)
 async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
     # 指定したい時刻から9時間引いた時刻
-    # if now == '15:30':
+    if now != '15:30':
+        return
     for ch in channel_sent.guild.voice_channels:
         for member in ch.members:
-            # move_to(None)で特定のメンバーを切断する
-            if member.id == 905072543919116308:
-                await member.move_to(None)
-                await channel_sent.send(f'{member.mention} おやすみ')
+            if member.id != 905072543919116308:
+                return
+            await member.move_to(None)
+            await channel_sent.send(f'{member.mention} おやすみ')
 
 @client.event
 async def on_ready():
