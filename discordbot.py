884f7490744e7cb6b300774b1c0288253cf62afe
maki <71871601+makkenno@users.noreply.github.com>
2022-08-14 14:18:54 +0900

    readyしてから実行開始にする

--- discordbot.py
+++ discordbot.py
@@ -8,18 +8,23 @@ import traceback
 CHANNEL_ID = '1008227908617248802'
 
 client = discord.Client()
-    
+
+channel_sent = None
+
 # 60秒に一回ループ
 @tasks.loop(seconds=10)
 async def loop():
     # 現在の時刻
     # now = datetime.now().strftime('%H:%M')
     # if now == '07:00':
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('10秒経ったよ')  
+    await channel_sent.send('10秒経ったよ') 
 
-#ループ処理実行
-loop.start()
+@client.event
+async def on_ready():
+    global channel_sent 
+    channel_sent = client.get_channel(CHANNEL_ID)
+    loop.start() #定期実行するメソッドの後ろに.start()をつける
+
 
 
 token = getenv('DISCORD_BOT_TOKEN')
