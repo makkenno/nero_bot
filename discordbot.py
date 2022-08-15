7b538a901f1128f95695c7875f8c271a9657a71a
maki <71871601+makkenno@users.noreply.github.com>
2022-08-16 00:50:27 +0900

    12:30に落ちるように修正

--- discordbot.py
+++ discordbot.py
@@ -20,8 +20,8 @@ async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
     # 指定したい時刻から9時間引いた時刻
-    #if now != '15:30':
-        #return
+    if now != '15:30':
+        return
     for ch in channel_sent.guild.voice_channels:
         for member in ch.members:
             if member.id not in target_members:
