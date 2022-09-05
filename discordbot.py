fa161ddbbe76d764da236eb3a885a56b6056ea17
maki <71871601+makkenno@users.noreply.github.com>
2022-09-06 00:03:21 +0900

    動作確認

--- discordbot.py
+++ discordbot.py
@@ -12,20 +12,20 @@ client = discord.Client()
 
 channel_sent = None
 
-target_members = [905072543919116308, 642474473802694687, 802487731786874880]
+target_members = [905072543919116308, 642474473802694687, 802487731786874880, 947106212674175027]
 # 指定したい時刻から9時間引いた時刻
 target_time =  ['13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00','18:30','19:00','19:30','20:00','20:30']
 
 
 
 # 60秒に一回ループ
-@tasks.loop(seconds=60)
+@tasks.loop(seconds=30)
 async def loop():
     # 現在の時刻
     target_member = random.choice(target_members)
     now = datetime.now().strftime('%H:%M')
-    if not (now in target_time):
-        return
+    #if not (now in target_time):
+    #   return
     for ch in channel_sent.guild.voice_channels:
         for member in ch.members:
             if member.id == target_member:
